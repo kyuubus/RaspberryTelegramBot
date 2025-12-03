48d51056ceb6ea53dbf7605eba1771b246ed657c
kyuubus <114469439+kyuubus@users.noreply.github.com>
2025-12-03 10:58:27 +0700

    Update Telepot.py

--- Telepot Raw/Telepot.py
+++ Telepot Raw/Telepot.py
@@ -1,5 +1,4 @@
 import telepot
-chat_id = 5443890447
-bot = telepot.Bot('7972098389:AAGyJnB1JJPkVI8QHd8_28tYCndJyrVdwMM')
+chat_id = 54430447  #edit this with your own chat id
+bot = telepot.Bot('7972098389:AAGyJnB1JJPkVI8QHd8_28tYCndJyr') #edit this with your own Telegram bot token
 bot.sendMessage(chat_id, 'start')
-print("Telegram bot is ready")
